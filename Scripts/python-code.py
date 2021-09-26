9fca92eaf0b59cb69f9cfe90e00cd93c6af52836
DarkWolf2244 <73813133+DarkWolf2244@users.noreply.github.com>
2021-09-26 16:19:52 +0530

    Update python-code.py

--- Scripts/python-code.py
+++ Scripts/python-code.py
@@ -1,6 +1,15 @@
+from rich import print
+from random import choice
+
+prefixes = [
+  "amazing",
+  "brilliant",
+  "smart",
+  "creative"
+]
 amazing_users = [
   "DarkWolf"
 ]
 
 for amazing_user in amazing_users:
-  print(amazing_user)
+  print(f"[b]The {choice(prefixes)} [green]{amazing user}[/green][/b]")
